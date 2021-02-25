c9cb5d242961543cfee935f8a8e473bb969f5952
Scott Stopinski <sstopin@gmail.com>
2021-02-24 17:51:59 -0800

    style: changed displayed text to youngest year of birth

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -211,7 +211,7 @@ def user_stats(df):
     # Display earliest, most recent, and most common year of birth
     if 'Birth_Year' in df:
         print('\Oldest year of birth:', int(df['Birth_Year'].min()))
-        print('\nMost recent year of birth:', int(df['Birth_Year'].max()))
+        print('\nYoungest year of birth:', int(df['Birth_Year'].max()))
 
         birth_df = df.groupby('Birth_Year').count()
 
